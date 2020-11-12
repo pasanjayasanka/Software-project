1b1eb902cef640c1deffa2ce319cb52b24a32a03
pasan <pasanjayasanka7@gmail.com>
2020-11-13 00:54:45 +0530

    Commit Test 2

---
+++ Food Application/Food Application/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Food_Application.Models
+{
+    public class Admin
+    {
+
+    }
+}
